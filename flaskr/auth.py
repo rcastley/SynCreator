97b7bbebfb30eec681bd07c3a35e015d1e4a4654
Robert Castley <robert.castley@gmail.com>
2021-02-22 16:35:17 +0000

    Added condition pages

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -14,7 +14,7 @@ def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        condition = 'default'
+        condition = 'Default'
 
         db = get_db()
         error = None
