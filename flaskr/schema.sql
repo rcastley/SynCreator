ed935d5e1f4b798c1560fddcca71a878bf9478a1
Robert Castley <robert.castley@gmail.com>
2021-03-24 19:04:41 +0000

    Added fields for control groups

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -4,5 +4,7 @@ CREATE TABLE scc (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   username TEXT UNIQUE NOT NULL,
   password TEXT NOT NULL,
-  condition TEXT NOT NULL
+  condition TEXT NOT NULL,
+  control_group TEXT,
+  post_token TEXT
 );
