6d87f6a9121be9b64eb2bf0434ad8e9a605961ae
Robert Castley <robert.castley@gmail.com>
2024-11-12 18:56:19 +0000

    Add default username/password

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -9,3 +9,5 @@ CREATE TABLE scc (
   rum_token TEXT,
   ingest_token TEXT
 );
+
+INSERT INTO scc (username, password, condition) VALUES ("demo", "scrypt:32768:8:1$tXMhCEjielkxym0O$049576219980b28a0ca487ed3ceddde67b10dc4e031e2721f8b53aab09fae53c9441087c9c3b3203a7311be2eec5894a23ca59aae3d3f5a4766ecd5430fbfc15", "default");
