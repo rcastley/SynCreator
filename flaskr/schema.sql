bbe0f66712a43e882686716ae25508d8776e03cf
Robert Castley <robert.castley@gmail.com>
2025-02-28 10:05:35 +0000

    Support for hec_token and url

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -7,7 +7,9 @@ CREATE TABLE scc (
   condition TEXT NOT NULL,
   realm TEXT,
   rum_token TEXT,
-  ingest_token TEXT
+  ingest_token TEXT,
+  hec_url TEXT,
+  hec_token TEXT
 );
 
 INSERT INTO scc (username, password, condition) VALUES ("demo", "scrypt:32768:8:1$tXMhCEjielkxym0O$049576219980b28a0ca487ed3ceddde67b10dc4e031e2721f8b53aab09fae53c9441087c9c3b3203a7311be2eec5894a23ca59aae3d3f5a4766ecd5430fbfc15", "default");
